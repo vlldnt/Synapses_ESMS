964d44b4b7edefa7595cb13ee0b0aa9e275041b8
vlldnt <vieilledent.adrien@gmail.com>
2026-03-26 17:14:34 +0100

    Add initial HTML template for frontend application

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
